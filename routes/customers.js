f12d0adf7c156514a27184371cb6f99413506e1e
Kibu-ero <114171012+Kibu-ero@users.noreply.github.com>
2025-12-01 13:32:19 +0800

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const customerController = require("../src/controllers/CustomerController");
+const customerController = require("../backend/src/controllers/CustomerController");
 
 // Get all customers
 router.get("/", customerController.getAllCustomers);
